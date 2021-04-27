b6f8c4686f4b1a114596676bc0d5704002df7f9b
praningnakooo <ballenasfra.ne08@gmail.com>
2021-04-27 18:18:58 +0800

    edit sidebar lag

--- src/app/shared/service/view-item.service.ts
+++ src/app/shared/service/view-item.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { from, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { Products } from "src/app/model/Product";
 import { ErrorHandlerService } from "./error-handler.service";
 import { catchError, tap } from "rxjs/operators";
