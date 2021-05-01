ae480880aa0fe08b065f0c5b421a42ae86ab34f4
joawa10 <joawa10@yahoo.com>
2021-05-01 21:08:41 +0800

    delete quotation and enquiry

--- src/app/model/enquiry/EnquiryDetails.model.ts
+++
@@ -1,9 +0,0 @@
-export class EnquiryDetails {
-  enquiryItemCode: Number;
-  enquiryDetailTsCreated: string;
-  itemName: string;
-  itemPeriod: string;
-  itemRate: string;
-  itemQty: string;
-  deletedFlag: string;
-}
